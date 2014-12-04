66abeefb6ba6348de8452754a067b36bb19b5a6d
danielobembe <ayo.obembe@gmail.com>
2014-12-04 13:50:45 +0000

    modified some stuff

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -3,8 +3,11 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
 	var searchResource = $resource('https://api.github.com/search/users');
 
   $scope.doSearch = function() {
-    $scope.$apply();
-    $scope.searchResult = searchResource.get({q: $scope.searchTerm });
+    // $scope.$apply();
+    $scope.searchResult = searchResource.get({
+    	q: $scope.searchTerm,
+    	access_token: "521f883ea1edb29c2d43c1f5091afc8cfc79ea62" 
+    });
 
 	};
 
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -22,7 +22,7 @@ describe('GitUserSearchController', function(){
 		beforeEach(inject(function($httpBackend) {
 			httpBackend = $httpBackend
 			httpBackend
-				.when("GET", "https://api.github.com/search/users?q=hello")
+				.when("GET", "https://api.github.com/search/users?access_token=521f883ea1edb29c2d43c1f5091afc8cfc79ea62&q=hello")
 				.respond({
 					items: items
 				});
@@ -41,11 +41,40 @@ describe('GitUserSearchController', function(){
 	  it('should display search results', function() {
 	  	scope.searchTerm = 'hello';
 	  	scope.doSearch();
-	  	scope.$apply();
+	  	// scope.$apply();
 	  	httpBackend.flush();
 	    expect(scope.searchResult.items).toEqual(items);
 	  });
 
+	});
+
+	describe('having clicked on a user', function(){
+
+		var httpBackend;
+		beforeEach(inject(function($httpBackend) {
+			httpBackend = $httpBackend
+			httpBackend
+				.when("GET", "https://api.github.com/search/users?access_token=521f883ea1edb29c2d43c1f5091afc8cfc79ea62&q=Callisto13")
+				.respond({
+					items: items
+				});
+			}));
+
+		  var items = [{
+      "login": "Callisto13",
+      "avatar_url": "https://avatars.githubusercontent.com/u/8898786?v=3",
+      "html_url": "https://github.com/Callisto13"
+  }];
+
+		it('it should pull up users github page', function(){
+			scope.searchTerm = 'Callisto13';
+	  	scope.doSearch();
+	  	// scope.$apply();
+	  	httpBackend.flush();
+	    expect(scope.searchResult.items).toEqual(items);
+	    // expect(scope.searchResult.items["avatar_url"]).toEqual("https://avatars.githubusercontent.com/u/8898786?v=3");
+		});
+			
 	});
 
 });
