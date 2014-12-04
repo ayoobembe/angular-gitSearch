0b0b97836b8182379da7141069eb9cad79b6a380
danielobembe <ayo.obembe@gmail.com>
2014-12-04 12:06:09 +0000

    Added access token

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -5,7 +5,7 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
   $scope.doSearch = function() {
     $scope.$apply();
     $scope.searchResult = searchResource.get({
-      q: $scope.searchTerm
+      q: $scope.searchTerm + '?access_token=521f883ea1edb29c2d43c1f5091afc8cfc79ea62'
     });
 
 	};
